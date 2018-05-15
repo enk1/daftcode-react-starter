b720612c58258ba778e180b6baaa22ac340496f2
enk1 <grzywaczewski.jakub@gmail.com>
2018-05-15 09:35:24 +0200

    LaunchesList view created

--- src/App.js
+++ src/App.js
@@ -1,31 +1,64 @@
 import { hot } from "react-hot-loader"
 import * as React from "react"
 import "./styles/theme.sass"
+
 import launch from "./assets/launch.json"
 import launchSite from "./assets/launch_site.json"
 import rocket from "./assets/rocket.json"
 import LaunchDetails from "view/LaunchDetails"
 
+import launches from "./assets/launches.json"
+import LaunchesList from "view/LaunchesList"
+
 class App extends React.Component {
+    // eslint-disable-line react/prefer-stateless-function
     constructor(props) {
         super(props)
         this.state = {
-            launch: launch,
-            launchSite: launchSite,
-            rocket: rocket,
+            viewName: "list",
+        }
+
+        this.handleLaunchClick = this.handleLaunchClick.bind(this)
+        this.handleBackClick = this.handleBackClick.bind(this)
+    }
+
+    get activeViewComponent() {
+        const { viewName } = this.state
+
+        switch (viewName) {
+            case "list":
+                return (
+                    <LaunchesList
+                        launches={launches}
+                        onLaunchClick={this.handleLaunchClick}
+                    />
+                )
+
+            case "details":
+                return (
+                    <LaunchDetails
+                        launch={launch}
+                        launchSite={launchSite}
+                        rocket={rocket}
+                        onBackClick={this.handleBackClick}
+                    />
+                )
+
+            default:
+                return null
         }
     }
 
+    handleLaunchClick() {
+        this.setState({ viewName: "details" })
+    }
+
+    handleBackClick() {
+        this.setState({ viewName: "list" })
+    }
+
     render() {
-        return (
-            <main>
-                <LaunchDetails
-                    launch={launch}
-                    launchSite={launchSite}
-                    rocket={rocket}
-                />
-            </main>
-        )
+        return <main>{this.activeViewComponent}</main>
     }
 }
 
---
+++ src/view/LaunchesList.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import "./LaunchDetails.sass"
+import { format, toUpper } from "date-fns"
+import Counter from "../components/Counter"
+
+class LaunchesList extends Component {
+    constructor() {
+        super()
+        this.state = {
+            launch: null,
+        }
+    }
+    render() {
+        return <div>s</div>
+    }
+}
+
+export default LaunchesList
